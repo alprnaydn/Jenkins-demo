c5479d154038e00c0981c33ad3e91dca1332b0f5
ALPEREN AYDIN <aydin.alperen_01@metu.edu.tr>
2024-07-09 12:16:22 +0300

    deneme

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ import (
 
 func main() {
 	router := gin.Default()
-	router.GET("/text", func(c *gin.Context) { c.String(200, "Hello Woooorld") })
+	router.GET("/text", func(c *gin.Context) { c.String(200, "Hello Woorld") })
 
 	router.Run(":5000")
 }
