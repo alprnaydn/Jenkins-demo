646adc857e814027e0b123c692b5e6c1bc67969d
ALPEREN AYDIN <126410289+alprnaydn@users.noreply.github.com>
2024-07-09 09:52:56 +0300

    Update main.go

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ import (
 
 func main() {
 	router := gin.Default()
-	router.GET("/text", func(c *gin.Context) { c.String(200, "Hello Wooorld") })
+	router.GET("/text", func(c *gin.Context) { c.String(200, "Hello World") })
 
 	router.Run(":5000")
 }
